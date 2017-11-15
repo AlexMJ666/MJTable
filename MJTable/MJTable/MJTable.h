2c8590ad27ff269f331fbc7575f08052c14f5195
AlexMJ666 <921711730@qq.com>
2017-11-15 15:32:44 +0800

    添加tableview

---
+++ MJTable/MJTable/MJTable.h
@@ -0,0 +1,14 @@
+//
+//  MJTable.h
+//  MJTable
+//
+//  Created by Mac on 2017/11/15.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#define kMainScreen_Height [UIScreen mainScreen].bounds.size.height
+#define kMainScreen_Width [UIScreen mainScreen].bounds.size.width
+@interface MJTable : UITableView
+
+@end
--- MJTable/MJTable/ViewController.m
+++ MJTable/MJTable/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "MJTable.h"
 @interface ViewController ()
 
 @end
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    MJTable* table = [[MJTable alloc]initWithFrame:CGRectMake(0, 0,kMainScreen_Width, kMainScreen_Height)];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
